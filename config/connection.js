cb1cbaa93a3447a3c45d0f1ed0104ea591ba618f
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-10-10 20:50:17 -0700

    fix connection.js to deplooy on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-require("dotenv").config();
+let sequelize;
 
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
-
-module.exports = sequelize;
+if (process.env.JAWDB_URL) {
+  sequelize = new Sequelize (process.env.JAWDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.BD_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect:'mysql',
+      port:3306,
+    },
+  );
+}
